9b18a08b0ac3ea88fb48b25a43462092ecabc444
Alexey Kozma <alexeykozma@gmail.com>
2021-04-18 21:55:12 +0500

    add  function main

--- after_install.sh
+++ after_install.sh
@@ -1,1 +1,22 @@
 #!/usr/bin/env bash
+
+main() 
+{
+    echo '----------------------Настойка после установки-------------------------'
+    echo '1) Обновление системы...'
+    sudo apt update && sudo apt dist-upgrade
+    echo '2) подключение репозиториев Debian...'
+    sudo apt install debian-archive-keyring dirmngr
+    sudo apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
+    sudo echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
+    sudo echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
+    echo "3) Обновление ..."
+    sudo apt update
+    echo "4) установка программ ..."
+    sudo apt install gcc g++ gdb make cmake git wget curl snapd
+    sudo snap install core 
+    echo "Установка завершена..."
+    return 0
+}
+
+main
