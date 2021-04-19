b2b5743b22ea2e76792ce3e3fc1737e3ab79aaaf
Alexey Kozma <alexeykozma@gmail.com>
2021-04-19 11:18:48 +0500

    after install update 2

--- after_install.sh
+++ after_install.sh
@@ -4,14 +4,14 @@ main()
 {
     echo '----------------------Настойка после установки-------------------------'
     echo '1) Обновление системы...'
-    sudo apt update && sudo apt dist-upgrade
+    sudo su apt update && apt dist-upgrade
     echo '2) подключение репозиториев Debian...'
-    sudo apt install debian-archive-keyring dirmngr
-    sudo apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
-    sudo echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
-    sudo echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
+    apt install debian-archive-keyring dirmngr
+    apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
+    echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
+    echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
     echo "3) Обновление ..."
-    sudo apt update
+    apt update && exit
     echo "4) установка программ ..."
     sudo apt install gcc g++ gdb make cmake git wget curl snapd
     sudo snap install core 
