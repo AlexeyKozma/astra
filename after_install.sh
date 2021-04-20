c0fc30eada13746f9b33319b5ebf0ffd94193880
Alexey Kozma <alexeykozma@gmail.com>
2021-04-20 18:13:58 +0500

    afret install update 5

--- after_install.sh
+++ after_install.sh
@@ -7,8 +7,8 @@ main()
     echo '2) подключение репозиториев Debian...'
     sudo apt install debian-archive-keyring dirmngr
     sudo apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
-    sudo echo '"deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list'
-    sudo echo '"deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list' 
+    sudo echo "'deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free' > /etc/apt/sources.list"
+    sudo echo "'deb https://mirror.yandex.ru/debian/ stretch main contrib non-free' >> /etc/apt/sources.list"
     echo "3) Обновление ..."
     sudo apt update
     echo "4) установка программ ..."
