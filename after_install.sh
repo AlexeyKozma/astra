dc737255dd3dbcbaa30084a5a965a1df66cf51e4
Alexey Kozma <alexeykozma@gmail.com>
2021-04-20 17:59:44 +0500

    after_install update 3

--- after_install.sh
+++ after_install.sh
@@ -2,22 +2,15 @@
 
 main() 
 {
-    echo '----------------------Настойка после установки-------------------------'
-    if [ "$EUID" -ne 0 ] ; then
-        echo "Для работы скрипта требуются права 'root'"
-        sudo su
-    fi
-    if [ "$EUID" -eq 0 ] ; then
-        echo '1) Обновление системы...'
-        apt update && apt dist-upgrade
-        echo '2) подключение репозиториев Debian...'
-        apt install debian-archive-keyring dirmngr
-        apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
-        echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
-        echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
-        echo "3) Обновление ..."
-        apt update && exit
-    fi
+    echo '1) Обновление системы...'
+    sudo apt update && apt dist-upgrade
+    echo '2) подключение репозиториев Debian...'
+    sudo apt install debian-archive-keyring dirmngr
+    sudo apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
+    sudo echo '"deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list'
+    sudo echo '"deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list' 
+    echo "3) Обновление ..."
+    sudo apt update
     echo "4) установка программ ..."
     sudo apt install gcc g++ gdb make cmake git wget curl snapd
     sudo snap install core 
