8fca917970906a4c2d14e4ed8dff5aa68fa0fd81
AlexeyKozma <46000958+AlexeyKozma@users.noreply.github.com>
2021-06-01 16:08:26 +0500

    Update 2

--- after_install.sh
+++ after_install.sh
@@ -5,11 +5,11 @@ main()
     echo '----------------------Настойка после установки-------------------------'
     if [[ $(id -u) -eq 0 ]] ; then
         echo '1) Обновление системы...'
-        apt update && sudo apt dist-upgrade
+        apt update && apt dist-upgrade
         echo '2) подключение репозиториев Debian...'
         apt install debian-archive-keyring dirmngr
         apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
-        echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" >> /etc/apt/sources.list
+        echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
         echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
         echo "3) Обновление ..."
         apt update
