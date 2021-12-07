e03dc09cd01d87e8f69beafdf301723422447e8b
AK <46000958+ak-1980@users.noreply.github.com>
2021-12-07 20:39:56 +0500

    alfa_01

--- after_install.sh
+++ after_install.sh
@@ -1,22 +1,43 @@
 #!/usr/bin/env bash
 
-main() 
-{
+list_e=()
+list_i=()
+list_i+=("gcc" "g++" "make" "cmake" "gdb" "git" "wget" "curl")
+upgrade_s () {
+    local up_=$(apt update && apt dist-upgrade)
+}
+
+add_soft() {
+    for i in "${list_i[@]}" 
+    do    
+        local res_=$(apt install $i)
+    done
+}
+
+add_yandex() {
+    echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
+    echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta main" >> /etc/apt/sources.list.d/yandex-browser.list
+    wget https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG && sudo apt-key add YANDEX-BROWSER-KEY.GPG
+    source /opt/yandex/browser-beta/update-ffmpeg
+    upgrade_s
+}
+
+set_base() {
+    echo '----------------------Настойка источников-------------------------'
+    echo '-----------------подключение репозиториев Debian...'
+    apt install debian-archive-keyring dirmngr
+    apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
+    echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
+    echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list
+    upgrade_s
     echo '----------------------Настойка после установки-------------------------'
-    if [[ $(id -u) -eq 0 ]] ; then
-        echo '1) Обновление системы...'
-        apt update && apt dist-upgrade && apt install git wget curl
-        echo '2) подключение репозиториев Debian...'
-        apt install debian-archive-keyring dirmngr
-        apt-key adv --recv-keys --keyserver keys.gnupg.net EF0F382A1A7B6500
-        echo "deb https://download.astralinux.ru/astra/testing/orel/repository orel contrib main non-free" > /etc/apt/sources.list
-        echo "deb https://mirror.yandex.ru/debian/ stretch main contrib non-free" >> /etc/apt/sources.list 
-        echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta main" >> /etc/apt/sources.list.d/yandex-browser.list
-        wget https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG && sudo apt-key add YANDEX-BROWSER-KEY.GPG
-        echo "3) Обновление ..."
-        apt update
-        echo "4) установка программ ..."
-        apt install yandex-browser-beta && {
+    sleep 3
+}
+
+
+set_lib() {
+    echo '----------------------Установка пакетов-------------------------'
+     local f_=$(
             mkdir /tmp/install_glibc/
             cd /tmp/install_glibc/
             wget http://ftp.ru.debian.org/debian/pool/main/g/glibc/libc6_2.28-10_amd64.deb
@@ -30,10 +51,26 @@ main()
             dpkg -i *
             cd ~
             rm -rf /tmp/install_glibc/
-            source /opt/yandex/browser-beta/update-ffmpeg
-        }
-        apt install gcc g++ gdb make cmake snapd
-        snap install core
+            upgrade_s
+         )
+    echo '----------------------Настойка после установки-------------------------'
+    sleep 3
+}
+
+main() 
+{
+    if [[ $(id -u) -eq 0 ]] ; then
+    while [[ "$#" -gt 0 ]] 
+    do
+        case "$1" in 
+        'base') set_base ;;
+        'lib') set_lib ;;
+        'yandex') add_yandex ;;
+        'soft') add_soft ;;
+        *) echo "Некоректные параметры!" ;;
+        esac
+        shift    
+    done    
     else 
         echo -e "Для работы скрипта требуются права 'root' !"
     fi
@@ -41,4 +78,4 @@ main()
     return 0
 }
 
-main
+main "$@"
