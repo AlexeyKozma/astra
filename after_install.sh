41bea3201dc2112efb3dbf3ee44edffeb06fadad
AK <46000958+ak-1980@users.noreply.github.com>
2022-09-22 14:50:44 +0500

    add docker

--- after_install.sh
+++ after_install.sh
@@ -106,6 +106,21 @@ install_fm () {
     sleep 1
 }
 
+docker() {
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg2 
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" | sudo tee -a /etc/apt/sources.list
+    sudo apt update
+    sudo apt install docker-ce
+    if [[ $(docker --version) ]] ; then 
+        sudo usermod -aG docker ${USER}
+        sudo su - ${USER}
+        docker run hello-world
+        sudo curl -L https://github.com/docker/compose/releases/download/2.11.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+        sudo chmod +x /usr/local/bin/docker-compose
+    fi
+}
+
 main() 
 {
     if [[ $(id -u) -eq 0 ]] ; then
@@ -118,6 +133,7 @@ main()
         'soft') add_soft ;;
         'i3') install_i3 ;;
         'fm') install_fm ;;
+        'docker') docker ;;
         *) echo "Некорректные параметры!" ;;
         esac
         shift    
